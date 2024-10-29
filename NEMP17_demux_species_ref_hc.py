bb431f319ec924861a2cd91657e73b70ba180aea
reedmcmullen <54691534+reedmcmullen@users.noreply.github.com>
2024-10-29 11:38:22 -0700

    Update NEMP17_demux_species_ref_hc.py

--- NEMP17_demux_species_ref_hc.py
+++ NEMP17_demux_species_ref_hc.py
@@ -1,10 +1,6 @@
 ./demux_species_ref.py \
---k=32 \
---out=human_chimp_k32 \
---names=human chimp \
---fasta=\
-/wynton/group/pollen/genomes/cellranger/refdata-gex-GRCh38-2024-A/fasta/genome.fa \
-/wynton/group/pollen/genomes/cellranger-arc/Chimp_gencodev33/fasta/genome.fa \
---gtf=\
-/wynton/group/pollen/genomes/cellranger/refdata-gex-GRCh38-2024-A/genes/genes.gtf.gz \
-/wynton/group/pollen/genomes/cellranger-arc/Chimp_gencodev33/genes/genes.gtf.gz \
+-k=32 \
+-o=human_chimp_k32 \
+-n=human chimp \
+-f=/wynton/group/pollen/genomes/cellranger/refdata-gex-GRCh38-2024-A/fasta/genome.fa /wynton/group/pollen/genomes/cellranger-arc/Chimp_gencodev33/fasta/genome.fa \
+-g=/wynton/group/pollen/genomes/cellranger/refdata-gex-GRCh38-2024-A/genes/genes.gtf.gz /wynton/group/pollen/genomes/cellranger-arc/Chimp_gencodev33/genes/genes.gtf.gz
