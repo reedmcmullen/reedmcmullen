cf277f80c002495ca7f346e976caf06b0472b0b9
reedmcmullen <54691534+reedmcmullen@users.noreply.github.com>
2024-10-28 17:29:30 -0700

    Create NEMP17_demux_species_ref_hc.py

---
+++ NEMP17_demux_species_ref_hc.py
@@ -0,0 +1,10 @@
+./demux_species_ref.py \
+--k=32 \
+--out=human_chimp_k32_kmers \
+--names=human chimp \
+--fasta=\
+/wynton/group/pollen/genomes/cellranger/refdata-gex-GRCh38-2024-A/fasta/genome.fa \
+/wynton/group/pollen/genomes/cellranger-arc/Chimp_gencodev33/fasta/genome.fa \
+--gtf=\
+/wynton/group/pollen/genomes/cellranger/refdata-gex-GRCh38-2024-A/genes/genes.gtf.gz \
+/wynton/group/pollen/genomes/cellranger-arc/Chimp_gencodev33/genes/genes.gtf.gz \
