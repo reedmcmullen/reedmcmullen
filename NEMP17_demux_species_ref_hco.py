6b85d7a7b064f1f2f7c365fd13cfb23c31a52774
reedmcmullen <54691534+reedmcmullen@users.noreply.github.com>
2024-10-30 12:00:20 -0700

    Update NEMP17_demux_species_ref_hco.py

--- NEMP17_demux_species_ref_hco.py
+++ NEMP17_demux_species_ref_hco.py
@@ -1,11 +1,11 @@
 ./demux_species_ref.py \
---k 32 \
---out human_chimp_orangutan_k32 \
---names human chimp orangutan \
---fasta /wynton/group/pollen/genomes/cellranger/refdata-gex-GRCh38-2024-A/fasta/genome.fa \
+-k 32 \
+-o human_chimp_orangutan_k32 \
+-n human chimp orangutan \
+-f /wynton/group/pollen/genomes/cellranger/refdata-gex-GRCh38-2024-A/fasta/genome.fa \
 /wynton/group/pollen/genomes/cellranger-arc/Chimp_gencodev33/fasta/genome.fa \
 /wynton/group/pollen/genomes/cellranger-arc/Orangutan_gencodev33/fasta/genome.fa \
---gtf=wynton/group/pollen/genomes/cellranger/refdata-gex-GRCh38-2024-A/genes/genes.gtf.gz \
+-g /wynton/group/pollen/genomes/cellranger/refdata-gex-GRCh38-2024-A/genes/genes.gtf.gz \
 /wynton/group/pollen/genomes/cellranger-arc/Chimp_gencodev33/genes/genes.gtf.gz \
 /wynton/group/pollen/genomes/cellranger-arc/Orangutan_gencodev33/genes/genes.gtf.gz
 
