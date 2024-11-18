c429681682a579216cb3328780865208e30fbacc
reedmcmullen <54691534+reedmcmullen@users.noreply.github.com>
2024-11-18 07:43:01 -0800

    Update NEMP17_clustering.py

--- NEMP17_clustering.py
+++ NEMP17_clustering.py
@@ -8,32 +8,40 @@ import anndata as ad
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+import random
 
 #Define and set the current working directory.
 directory_path = '/wynton/home/pollenlab/reedmcmullen/projects/NEMP17/scanpy_NEMP17'
 os.chdir(directory_path)
 
 #Read in full dataset as and AnnData object.
+print('Loading dataset...')
 anndata = directory_path + '/NEMP17.h5ad'
 adata = sc.read_h5ad(anndata)
 
-#KNN, UMAP, and leiden clustering.
+#Redo KNN and UMAP for full integrated dataset.
+print('Running neighbor finding...')
 sc.pp.neighbors(adata)
+print('Running UMAP...')
 sc.tl.umap(adata)
-res = 0.5
-sc.tl.leiden(adata, resolution=res)
 
-#Save dataset.
+#Redo clustering for full integrated dataset
+print('Running leiden clustering...')
+res = 0.75
+sc.tl.leiden(adata, flavor="igraph", n_iterations=2, resolution=res)
+
+#Save dataset
+print('Saving dataset...')
 anndata = directory_path + '/NEMP17.h5ad'
 adata.write(anndata, compression='gzip')
 
-#Subset the AnnData object to 10% of cells for faster testing and visualization.
-np.random.seed(42)
-n_cells = int(adata.n_obs * 0.1)
-random_indices = np.random.choice(adata.obs.index, size=n_cells, replace=False)
-adata_sub = adata[random_indices].copy()
+#Subset dataset to 100k cells.
+random.seed(0)
+adata_sub=adata[np.random.choice(adata.obs.index, 100000, replace=False),:]
 
-#Save subset dataset.
+#Save dataset subset.
+print('Saving dataset subset...')
 anndata_subset = directory_path + '/NEMP17_subset.h5ad'
-adata_sub.write(anndata_subset, compression='gzip')
+adata_sub.write(anndata_subset)
+
 
