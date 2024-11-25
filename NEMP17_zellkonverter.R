be274de34f3b36836f05a92b630f2ab44ac24a2d
reedmcmullen <54691534+reedmcmullen@users.noreply.github.com>
2024-11-25 15:12:10 -0800

    Update NEMP17_zellkonverter.R

--- NEMP17_zellkonverter.R
+++ NEMP17_zellkonverter.R
@@ -1,19 +1,20 @@
+#!/usr/bin/env Rscript
+#Before running script, install the required packages.
+#install.packages('remotes')
+#library(remotes)
+#remotes::install_github("satijalab/seurat", "seurat5")
+#if (!require("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("zellkonverter")
 
-#Install and load remotes packages.
-install.packages('remotes')
-library(remotes)
-
-#Install and load required packages.
-remotes::install_github("satijalab/seurat", "seurat5", quiet = TRUE)
-if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install("zellkonverter", version = "1.14.1")
+#Load the required packages.
 library(Seurat)
 library(zellkonverter)
+library(reticulate)
 
-#Read in AnnData object from H5AD file, with raw count data in adata.X.
+#Read in the AnnData object from H5AD file, with raw count data in adata.X.
 h5ad_file <- "/wynton/home/pollenlab/reedmcmullen/projects/NEMP17/dreamlet_NEMP17/NEMP17_counts.h5ad"
-adata <- readH5AD(h5ad_file, X_name='counts')
+adata <- readH5AD(h5ad_file, X_name='counts', reader='R')
 
 #Convert to RDS file
 rds_file <- "/wynton/home/pollenlab/reedmcmullen/projects/NEMP17/dreamlet_NEMP17/NEMP17_dreamlet.rds"
